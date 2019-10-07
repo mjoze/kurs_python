21ca2349dba2c54a6e0e85fec44d457556e01ae7
mjoze <mirek.jozefiak@gmail.com>
2019-10-07 20:02:07 +0200

    '1'

---
+++ 00/weeks.py
@@ -0,0 +1,9 @@
+weeks = 7
+print('Liczba minut w ciagu' , weeks , 'tygodni:' , weeks * (7 * 24 * 60))
+
+hours = 4
+learn = 7 * hours
+print('nową umiejetnosc zdobedziesz za' , 75 // learn , 'tygodnie')
+
+weight = 100
+height = 181
