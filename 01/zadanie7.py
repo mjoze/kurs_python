0c9005ed286a11c55d5c935b1a8e241606f36a37
mjoze <mirek.jozefiak@gmail.com>
2019-10-10 20:31:55 +0200

    zad7'

---
+++ 01/zadanie7.py
@@ -0,0 +1,7 @@
+'''Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową. Użyj funkcji format(),
+by wyświetlić zdanie zawierające te wartości.'''
+
+a = 'Mops'
+b = 10
+c = 'Mój pies to {} i waży {}'.format(a, b)
+print(c)
