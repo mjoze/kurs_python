9d3c268b7e01b6c0c9f079034c1c3b5984dce3cb
mjoze <mirek.jozefiak@gmail.com>
2019-10-15 20:09:48 +0200

    r

---
+++ 01_zmienne_i_typy/zadanie1.py
@@ -0,0 +1,4 @@
+#1
+word = 'ciasteczko'
+middle = len(word) // 2
+print(word[middle - 1:middle + 2])
---
+++ 01_zmienne_i_typy/zadanie3.py
@@ -0,0 +1,20 @@
+#3
+quote = 'Honesty is the first chapter in the book of wisdom.'
+# a
+print(len(quote))
+# b
+print(quote[-7:-1])
+# c
+middle = len(quote)//2
+print(quote[:middle])
+# d
+print(quote[-1])
+# e
+print(quote[middle::3])
+# f
+print(quote[::2])
+# g
+print(quote[::-1])
+# h
+print(quote.replace('wisdom', 'friendship'))
+
