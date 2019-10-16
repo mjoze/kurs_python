b9093d1fab149380a09efa8162ab83de291e5fcb
mjoze <mirek.jozefiak@gmail.com>
2019-10-16 11:12:46 +0200

    'enter'

--- 02_instrukcje_sterujace/for/for4.py
+++ 02_instrukcje_sterujace/for/for4.py
@@ -6,7 +6,7 @@ silnia = 1
 if a < 8:
     for i in range(1, 1 + a):
         silnia = silnia * i
-        print(silnia, i)
+        # print(silnia, i)
     print('silnia z {} wynosi {}'.format(a, silnia))
 else:
     print('podana liczba jest większa niż 8')
