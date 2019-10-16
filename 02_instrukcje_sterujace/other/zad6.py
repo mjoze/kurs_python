bb921cd706cafa5e1250c6902311c4ed610ad797
mjoze <mirek.jozefiak@gmail.com>
2019-10-16 19:46:56 +0200

    p

---
+++ 02_instrukcje_sterujace/other/zad6.py
@@ -0,0 +1,9 @@
+""""Wyświetl w konsoli klasyczną tabliczkę mnożenia. W razie wątpliwości jak sformatować,
+ by w konsoli pojawiła się tabela warto sobie przypomnieć """
+for i in range(0, 11):
+    for j in range(1, 11):
+        print('{}' .format(i*j), '|', end='')
+
+
+
+
---
+++ 03_kolekcje/set1.py
@@ -0,0 +1,6 @@
+"""Utwórz dowolną krotkę, w której elementy mogą się powtarzać. Przekształć ją w set."""
+
+a = ("a", "w", "reksio", "e", "e", 1, 1, "reksio")
+print(a)
+b = set(a)
+print(b)
