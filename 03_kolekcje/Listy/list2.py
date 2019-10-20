695700f9e45c530ca61416e266ae57b8672ce1fa
mjoze <mirek.jozefiak@gmail.com>
2019-10-20 19:26:59 +0200

    string_format

--- 03_kolekcje/Listy/list2.py
+++ 03_kolekcje/Listy/list2.py
@@ -3,7 +3,8 @@
 
 num_list = []
 for i in range(10):
-    x = int(input('Podaj liczbę'))
+    x = int(input('Podaj liczbę nr {}'.format(i+1)))
     if x % 2 == 0:
         num_list.append(x)
+num_list.sort()
 print(num_list)
