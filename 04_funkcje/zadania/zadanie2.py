e9862273458c1caa341f96507e537e50ee5f1262
mjoze <mirek.jozefiak@gmail.com>
2019-10-22 15:22:22 +0200

    '04_funkcje/zadania/zadanie2.py'

--- 04_funkcje/zadania/zadanie2.py
+++ 04_funkcje/zadania/zadanie2.py
@@ -2,22 +2,31 @@
 minimum_of(a, b, c)."""
 
 
+# def minimum_of(a, b, c):
+#     if a < b:
+#         i = a
+#         a = b
+#         b = i
+#     if a < c:
+#         i = a
+#         a = c
+#         c = i
+#     if b < c:
+#         i = b
+#         b = c
+#         c = i
+
+# return c
+
+
 def minimum_of(a, b, c):
-    if a < b:
-        i = a
-        a = b
-        b = i
-    if a < c:
-        i = a
-        a = c
-        c = i
-    if b < c:
-        i = b
-        b = c
-        c = i
-    # print(c)
-    return c
+    numbers_list = [a, b, c]
+    min_n = numbers_list[0]
+    for i in numbers_list:
+        if i < min_n:
+            min_n = i
+    return min_n
 
 
-x_min = minimum_of(487, 210, 890)
+x_min = minimum_of(487, 21, 890)
 print(x_min)
