cc24e574bcf105a9c3d85643e5933f635188e6a0
mjoze <mirek.jozefiak@gmail.com>
2019-10-22 15:17:04 +0200

    a_b_c

--- 04_funkcje/zadania/zadanie3.py
+++ 04_funkcje/zadania/zadanie3.py
@@ -1,11 +1,10 @@
 """ Nie korzystając z funkcji wbudowanej max(), napisz funkcję znajdującą maksymalną wartość z 3 liczb.
 maximum_of(a, b, c)."""
 
-x = [21, 3, 54, 98, 1, 0, 2, 3]
 
-
-def my_max(list_number):
+def my_max(a, b, c):
     """return the minimum element of sequence"""
+    list_number = [a, b, c]
     max_n = list_number[0]
     for i in list_number:
         if i > max_n:
@@ -13,5 +12,5 @@ def my_max(list_number):
     return max_n
 
 
-a = my_max(x)
+a = my_max(2, 1, 34)
 print(a)
