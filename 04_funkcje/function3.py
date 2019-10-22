7ed5855980036c04ff134444c2fe1a9ad35d8fc8
mjoze <mirek.jozefiak@gmail.com>
2019-10-22 14:42:29 +0200

    'function3'

--- 04_funkcje/function3.py
+++ 04_funkcje/function3.py
@@ -1,4 +1,14 @@
 """Napisać funkcję, która przyjmuje listę liczb i zwraca sumę wszystki elementów na liście."""
 
-numb_list = []
+numb_list = [1, 2, 3, 4, 8]
 
+
+def sum_numbers(numbers):
+    sum_n = 0
+    for i in numbers:
+        sum_n += i
+    return sum_n
+
+
+a = sum_numbers(numb_list)
+print(a)
