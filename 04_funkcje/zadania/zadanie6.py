5f4b1f91c82d82b8552a4af4e1429132644fae0c
mjoze <mirek.jozefiak@gmail.com>
2019-10-24 13:44:02 +0200

    added_game_def

--- 04_funkcje/zadania/zadanie6.py
+++ 04_funkcje/zadania/zadanie6.py
@@ -1,18 +1,39 @@
 """ Napisz grę kamień-papier-nożyce tak, aby korzystać z funkcji."""
+
+import random
+
 # loss : win
 rules = {
-    'n':  'k',
+    'n': 'k',
     'k': 'p',
     'p': 'n',
 }
 
-print(rules.keys())
-def declareWinner(user, ai):
-    if user == ai:
-        print("remis")
-    elif user in rules.keys() and ai in rules.values():
-        print("ai win")
-    else:
-        print("user win")
 
-declareWinner('n', 'n')
+def declare_winner(user, ai, ):
+    return (user, ai) if user == ai else (ai if (user in rules.keys() and ai == rules[user]) else user)
+
+
+def game(n):
+    result = {
+        'user': 0,
+        'ai': 0,
+        'draw': 0,
+    }
+    while n > 0:
+        user = input("podaj")
+        ai = random.choice(['p', 'k', 'n'])
+        print('komputer wybrał: {}'.format(ai))
+        score = declare_winner(user, ai)
+        if score == user:
+            print('wygrana')
+            result['user'] += 1
+        elif score == ai:
+            print('przegrana')
+            result['ai'] += 1
+        else:
+            print('remis')
+            result['draw'] += 1
+        n -= 1
+    return result
+
