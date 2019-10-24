9d46ef2fd68f23d077ed9ed21c6a3c66d1f8eaa0
mjoze <mirek.jozefiak@gmail.com>
2019-10-24 12:49:26 +0200

    a_if_cond_else_b

--- 04_funkcje/zadania/zadanie3.py
+++ 04_funkcje/zadania/zadanie3.py
@@ -3,7 +3,6 @@ maximum_of(a, b, c)."""
 
 
 def my_max(a, b, c):
-    """return the minimum element of sequence"""
     list_number = [a, b, c]
     max_n = list_number[0]
     for i in list_number:
@@ -12,5 +11,20 @@ def my_max(a, b, c):
     return max_n
 
 
-a = my_max(2, 1, 34)
+a = my_max(26, 190, 78)
 print(a)
+
+
+def max_of(a, b, c):
+    return max_of_2(max_of_2(a, b), c)
+
+
+def max_of_2(first, second):
+    return first if first > second else second
+
+
+x = 3
+y = 8
+z = 2
+result = max_of(x, y, z)
+print(result)
