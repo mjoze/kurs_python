61b79164b3de1b75a6a9b14319b4e24d92497301
mjoze <mirek.jozefiak@gmail.com>
2019-10-24 12:56:06 +0200

    modified_if

--- 02_instrukcje_sterujace/zad4.py
+++ 02_instrukcje_sterujace/zad4.py
@@ -28,26 +28,14 @@ while num_round > 0:
     ai_choice = random.choice(['k', 'p', 'n'])
     if user_choice == 'koniec':
         break
-    if user_choice == 'k' and ai_choice == 'p':
+    if (user_choice == "k" and ai_choice == "n") or (user_choice == 'p' and ai_choice == "k") \
+            or (user_choice == 'n' and ai_choice == 'k'):
+        win_user += 1
+        print(w)
+    else:
         print(p)
         win_ai += 1
-    elif user_choice == 'p' and ai_choice == 'k':
-        print(w)
-        win_user += 1
-    if user_choice == 'p' and ai_choice == 'n':
-        print(p)
-        win_ai += 1
-    elif user_choice == 'n' and ai_choice == 'p':
-        print(w)
-        win_user += 1
-    if user_choice == 'k' and ai_choice == 'n':
-        print(w)
-        win_user += 1
-    elif user_choice == 'n' and ai_choice == 'k':
-        print(p)
-        win_ai += 1
-    if user_choice == ai_choice:
-        print('remis')
+
     num_round -= 1
     print(win_user, win_ai)
     if win_user > win_ai:
