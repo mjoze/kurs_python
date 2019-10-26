cd0b02b7b5df65ae8ca3d3c7e8cd1105c96e2e5a
mjoze <mirek.jozefiak@gmail.com>
2019-10-26 14:27:01 +0200

    adressbook

---
+++ hackaton_1/adress_book.py
@@ -0,0 +1,67 @@
+"""### Książka adresowa:
+Program przechowujący danę kontaktowe znajomych/klientów.
+- Program wyświetla menu wypisujące komendy jakie należy wpisać, aby program wykonał dane zadanie. Zadania to:
+    - Wyświetlenie wszystkich wpisów
+    - Stworzenie nowego wpisu (dane wczytywane z klawiatury)
+    - Usunięcie wpisu
+    - Zakończenie pracy programu
+- Program powinien na starcie mieć już wprowadzone kilka wpisów."""
+
+import random
+
+book = {1: {'name': "Anna", 'surname': 'Kamińska', 'mail': 'anna.kaminska@gmail.com'},
+        2: {'name': "Marek", 'surname': "Płaski", 'mail': 'marek.plaski@gmial.com'},
+        }
+
+
+def view(x):
+    for k, v in book.items():
+        print(v)
+    return book
+
+
+def add_contact(x, y, z):
+    w = random.randrange(1, 1000)
+    book[w] = {'name': x, 'surname': y, 'mail': z}
+    return book
+
+
+def del_contact(txt):
+    n = 0
+    for k, v in book.items():
+        for i in v.values():
+            if i == txt:
+                n = k
+    if n == 0:
+        print('Error. Name not in adress book')
+    else:
+        book.pop(n)
+    return book
+
+
+def exit_program(q):
+    if q == 'Q':
+        print("koniec")
+    return
+
+
+start_app = True
+while start_app:
+    print("show adress book press A")
+    print("add new contact press B")
+    print('remove contact press R')
+    print("exit program press Q")
+    a = input("").upper()
+    if a == 'A':
+        view(book)
+    if a == 'B':
+        name = input("name:").capitalize()
+        surname = input('surname:').capitalize()
+        mail = input('email:')
+        add_contact(name, surname, mail)
+    if a == "R":
+        remove = input("contact to remove. name or surname").capitalize()
+        del_contact(remove)
+    if a == "Q":
+        exit_program(a)
+        start_app = False
