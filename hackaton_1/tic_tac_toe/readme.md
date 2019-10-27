fb353b3b2d5059224a73259e4eb9c8b8eb5b1718
mjoze <mirek.jozefiak@gmail.com>
2019-10-27 11:06:55 +0100

    tic_tac_toe_game

---
+++ hackaton_1/tic_tac_toe/readme.md
@@ -0,0 +1,14 @@
+## General info
+This project is simply game Tic Tac Toe.
+
+## Technologies
+Project is created with:
+* Python 3.7
+
+## How To Play
+* Choose the characters for yourself and computer
+* Select the position of the mark
+* the computer randomly selects the character
+* the winner is the first to lay three signs next to each other horizontally, vertically or diagonally
+
+
--- hackaton_1/tic_tac_toe/x_and_o.py
+++ hackaton_1/tic_tac_toe/x_and_o.py
@@ -1,5 +1,4 @@
-"""Stwórz gre w kółko i Krzyżyk dla 2 graczy. Zacznij od najważniejszej części – rozgrywki,
-a następnie dodaj menu, opcje takie jak imiona graczy, pomysły własne."""
+
 import random
 
 game = {'A1': '-', 'A2': '-', 'A3': '-', 'B1': '-', 'B2': '-', 'B3': '-', 'C1': '-', 'C2': '-', 'C3': '-'}
@@ -16,7 +15,7 @@ def choice_x(x, user_sign, ai_sign):
     user_sign = user_sign
     ai_sign = ai_sign
     if x not in game.keys():
-        print('Podałeś złą wartość')
+        print("You've given the wrong value")
         return
     elif x not in xo_list:
         game[x] = user_sign
@@ -30,21 +29,21 @@ def choice_x(x, user_sign, ai_sign):
         ai_choice.remove(o)
         return xo_list, game
     else:
-        print('To już było. Wybierz ponownie')
+        print("It's already been done. Select again")
 
 
 def print_game():
-    print('', "1", "2", "3")
+    print(' ', "1", "2", "3")
     print('A', game['A1'], game['A2'], game['A3'])
     print('B', game['B1'], game['B2'], game['B3'])
     print('C', game['C1'], game['C2'], game['C3'])
 
 
 print_game()
-user = input('user sign')
-ai = input("AI sign")
+user = input('Select your sign: ')
+ai = input("Select a computer sign: ")
 while True:
-    a = input('podaj pozycje {}'.format(user)).upper()
+    a = input("What's the position {}?".format(user)).upper()
     choice_x(a, user, ai)
     if x_win in game_wins:
         print('{} win'.format(user))
