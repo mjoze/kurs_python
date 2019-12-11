b2d94f1ebf5d79fc352b1a0d64e19c9198259f97
mjoze <mirek.jozefiak@gmail.com>
2019-12-11 17:08:02 +0100

    'fields_tests'

--- Testy/fields.py
+++ Testy/fields.py
@@ -1,8 +1,30 @@
+def check_values(*values):
+    for i in values:
+        if not isinstance(i, (int, float)):
+            raise ValueError(f'Bok {i} musi być wartością numeryczną')
+
+
 def rectangle(a, b):
+    check_values(a, b)
     return a * b
+    # pass
 
 
 def triangle(a, h):
+    check_values(a, h)
     return 0.5 * a * h
 
 
+def trapeze(a, b, h):
+    check_values(a, b, h)
+    return (a + b)/2 * h
+
+
+def main():
+    print(rectangle(2, 3))
+    print(triangle(2, 3))
+    print(trapeze(2, 3, 4))
+
+
+if __name__ == "main":
+    main()
--- Testy/test_fields.py
+++ Testy/test_fields.py
@@ -1,15 +1,39 @@
 import unittest
-from fields import rectangle, triangle
+from fields import rectangle, triangle, trapeze
 
 
 class FieldsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.a = 10
+        self.b = 50
+
     def test_rectangle_with_correct_values(self):
-        result = rectangle(5, 5)
-        self.assertEqual(result, 25)
+        result = rectangle(self.a, self.b)
+        self.assertEqual(result, 500)
+
+    def test_rectangle_with_incorrect_value(self):
+        with self.assertRaises(ValueError):
+            rectangle(5, 'aaa')
 
     def test_triangle_with_correct_values(self):
-        result = triangle(5, 5)
-        self.assertEqual(result, 12.5)
+        result = triangle(self.a, self.b)
+        self.assertEqual(result, 250)
+
+    def test_triangle_with_incorrect_value(self):
+        with self.assertRaises(ValueError):
+            triangle('sss', 3)
+
+    def test_trapeze_with_correct_value(self):
+        result = trapeze(10, 10, 2)
+        self.assertEqual(result, 20)
+
+    def test_trapeze_with_incorrect_value(self):
+        with self.assertRaises(ValueError):
+            trapeze(5, 'aaa')
+
+    def tearDown(self):
+        del self.a
+        del self.b
 
 
 if __name__ == '__main__':
