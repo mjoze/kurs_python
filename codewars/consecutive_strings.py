570f2964b03b78cbab055a2f91545dba041bd438
mjoze <mirek.jozefiak@gmail.com>
2019-12-18 19:10:09 +0100

    consecutive_strings

---
+++ codewars/consecutive_strings.py
@@ -0,0 +1,13 @@
+"""You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Example:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+Note
+consecutive strings : follow one after another without an interruption"""
+
+
+def longest_consec(s, k):
+    return max(["".join(s[i:i+k]) for i in range(len(s)-k+1)], key=len) if s and 0 < k <= len(s) else ""
