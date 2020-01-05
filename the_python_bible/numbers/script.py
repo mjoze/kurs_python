dcfca93fe0722bdeb99d71d384effa2275512680
mjoze <mirek.jozefiak@gmail.com>
2020-01-05 17:42:11 +0100

    'math'

--- the_python_bible/numbers/script.py
+++ the_python_bible/numbers/script.py
@@ -1,6 +1,7 @@
 # print(type(2))
 # print(type(0.2))
 import random
+import math
 
 health = 50
 easy = 1
@@ -10,3 +11,10 @@ difficulty = easy
 potion_health = int(random.randint(25, 50) / difficulty)
 health = health + potion_health
 print(health)
+
+print(math.pi)
+print(math.e)
+print(math.floor(2.1))
+print(math.ceil(2.1))
+print(math.pow(2, 2))
+print(2 ** 2)
