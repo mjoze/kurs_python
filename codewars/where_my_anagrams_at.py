3b9764a13bee836384f0674885ee58b14a071a50
mjoze <mirek.jozefiak@gmail.com>
2020-01-11 19:46:54 +0100

    'solutionwitwhileloop'

--- codewars/where_my_anagrams_at.py
+++ codewars/where_my_anagrams_at.py
@@ -13,3 +13,27 @@ an array with words. You should return an array of all the anagrams or an empty 
 anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
 anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
 anagrams('laser', ['lazing', 'lazy',  'lacer']) => []"""
+
+
+def anagrams(word, words):
+    word1 = []
+    word2 = []
+    results = []
+    for i in word:
+        word1.append(i)
+    word1.sort()
+    counter = 0
+    while True:
+        for i in words[counter]:
+            word2.append(i)
+        word2.sort()
+        if word1 == word2:
+            results.append(words[counter])
+        counter += 1
+        word2 = []
+        if counter == len(words):
+            return results
+
+
+"Clever"
+# def anagrams(word, words): return [item for item in words if sorted(item)==sorted(word)]
