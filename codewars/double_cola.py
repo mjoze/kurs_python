76cdbf684d6029440f5bae4aec5a36d090dcf148
mjoze <mirek.jozefiak@gmail.com>
2020-01-16 14:40:18 +0100

    'started_solution'

--- codewars/double_cola.py
+++ codewars/double_cola.py
@@ -1,4 +1,7 @@
-"""Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
+"""Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are
+no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two
+ Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end
+ of the queue as two Leonards, and so on.
 
 For example, Penny drinks the third can of cola and the queue will look like this:
 
@@ -13,4 +16,12 @@ Return the single line — the name of the person who drinks the n-th can of col
 
 who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 1) == "Sheldon"
 who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52) == "Penny"
-who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951) == "Leonard""""
+who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951) == "Leonard"""
+
+
+def who_is_next(names, r):
+    if r <= 5:
+        return names[r-1]
+    else:
+        a = r % 5
+        return names[a]
