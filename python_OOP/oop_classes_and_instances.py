bae268b00b457acbcc28da65d24ccb2785dc21cf
mjoze <mirek.jozefiak@gmail.com>
2020-01-20 18:51:51 +0100

    'employee'

--- python_OOP/oop_classes_and_instances.py
+++ python_OOP/oop_classes_and_instances.py
@@ -1,7 +1,17 @@
 class Employee:
-    pass
 
-emp_1 = Employee()
-emp_2 = Employee()
-print(emp_1)
-print(emp_2)
+    def __init__(self, first, last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first.lower() + '.' + last.lower() + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Pol', 'Madera', 78000)
+emp_2 = Employee('Test', 'User', 67000)
+
+print(emp_1.email)
+print(emp_1.fullname())
