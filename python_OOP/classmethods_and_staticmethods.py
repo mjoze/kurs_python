05a208887c243218f77f7d4d634511df47f58d4c
mjoze <mirek.jozefiak@gmail.com>
2020-01-21 15:46:59 +0100

    'classmethods_staticmethods'

---
+++ python_OOP/classmethods_and_staticmethods.py
@@ -0,0 +1,59 @@
+import datetime
+
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first.lower() + '.' + last.lower() + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+emp_1 = Employee('Pol', 'Madera', 78000)
+emp_2 = Employee('Test', 'User', 67000)
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doo-90000'
+
+# first, last, pay = emp_str_1.split('-')
+# new_emp_1 = Employee(first, last, pay)
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+print(new_emp_1.pay)
+# Employee.set_raise_amt(1.05)
+# emp_1.set_raise_amt(1.05)
+#
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+my_date = datetime.date(2020, 1, 25)
+print(Employee.is_workday(my_date))
