0c63db8762db8a6c9550a2dcb7d1fdd6b273596e
mjoze <mirek.jozefiak@gmail.com>
2020-02-01 19:58:20 +0100

    solved

---
+++ codewars/calculate_average.py
@@ -0,0 +1,13 @@
+
+"""
+Write function avg which calculates average of numbers in given list.
+
+Python:
+
+Due to rounding issues please do not use statistics.mean or such.
+If the array is empty, return 0.
+"""
+
+
+def find_average(arr):
+    return sum(arr) / len(arr)
