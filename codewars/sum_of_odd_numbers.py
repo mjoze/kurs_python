e2e1795a570122d1143eb534fcd5757a79a7d7f5
mjoze <mirek.jozefiak@gmail.com>
2020-02-02 17:36:49 +0100

    'solved'

---
+++ codewars/sum_of_odd_numbers.py
@@ -0,0 +1,31 @@
+"""
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+row_sum_odd_numbers(1); # 1
+row_sum_odd_numbers(2); # 3 + 5 = 8
+"""
+
+
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+
+"""
+def row_sum_odd_numbers(n):
+    m = 1
+    sum_arr = []
+    for i in range(n+1):
+        for _ in range(1, i+1):
+            if i == n:
+                sum_arr.append(m)
+            m+=2
+    return sum(sum_arr)
+"""
