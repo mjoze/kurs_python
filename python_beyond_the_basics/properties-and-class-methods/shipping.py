119701e25b5f8d674337ce31f4e85a7ff3374d31
mjoze <mirek.jozefiak@gmail.com>
2020-04-25 15:39:16 +0200

    static methods

--- python_beyond_the_basics/properties-and-class-methods/shipping.py
+++ python_beyond_the_basics/properties-and-class-methods/shipping.py
@@ -2,10 +2,15 @@ class ShippingContainer:
 
     next_serial = 1337
 
+    @staticmethod
+    def _get_next_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = self.next_serial
-        self.next_serial += 1
+        self.serial = ShippingContainer._get_next_serial()
 
 
